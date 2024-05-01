c551e5f1ddebcd6e3faaacc1463bb1033d8c2c00
lsilvas1 <160453425+lsilvas1@users.noreply.github.com>
2024-04-30 21:13:29 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+package com.umg.test;
+
+import java.util.Scanner;
+
+//import java.util.Scanner;
+
+import com.umg.dao.TestDao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Ingresar datos de forma manual 
+		//TestDao.estudiante("Santos", "Irene", "Santiago", "Lopez", "femenino", "205-02-02", "51306497", "2585306690103", "600.00", "lsilva");
+		//Solicitar ingresar los datos de los estudiantes
+		System.out.println("Ingresa los datos solicitados: Primer Nombre, Segundo Nombre, Primer Apellido, Segundo Apellido, Genero, Fecha de Nacimiento, Numero de Telefono, DPI, Precio Colegiatura, Usuario que Ingresa los datos, Debes presionar enter cuando ingreses cada uno de los datos:"); 
+		Scanner scanner = new Scanner(System.in);
+		
+		TestDao.estudiante(scanner.nextLine(),scanner.nextLine(),scanner.nextLine(),scanner.nextLine(),scanner.nextLine(),scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
+	}
+
+}
